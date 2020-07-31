d8e949a38dfb9eb0381665be4d9d5c2eec347291
projectscodeartssolution <projects@codeartssolution.com>
2020-07-31 21:18:03 +0530

    checkout component changes

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -4,6 +4,9 @@ import { Platform, LoadingController, ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { Router,ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../allServices/api.service';
+
+
 declare var RazorpayCheckout:any;
 @Component({
   selector: 'app-checkout',
@@ -11,11 +14,16 @@ declare var RazorpayCheckout:any;
   styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
+  user = this.api.getCurrentUser();
   todo: { 
     couponCode: string, 
    } = {
     couponCode: ''
   };
+  userid:any;
+  
+  userData:any;
+  couponAdded:string = 'false';
   cartProduct : any = JSON.parse(localStorage.getItem("product"))?JSON.parse(localStorage.getItem("product")):[];
   price:any;
   couponData:any;
@@ -53,21 +61,42 @@ export class CheckoutComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private http:HttpClient,
-    private storage: Storage
+    private storage: Storage,
+    private api: ApiService
   ) { 
     //this.storage.clear();
     console.log(this.cartProduct);
     this.price = this.cartProduct.price;
     this.storage.get('userInfo').then((val) => {
       let userInfo = JSON.parse(val);
-      console.log(userInfo);
+      console.log(userInfo.user_email);
       if(!userInfo){
         this.router.navigate(['/sign-in',{"routeParams":"checkout"}]);
       }
+
+      this.http.get('https://avatto.in/wp-json/avatto/v2/user-id/?ue='+userInfo.user_email).subscribe(data=>{
+        this.userData = data;
+        if(this.userData.id !="null"){
+          this.userid = this.userData.id;
+        }
+      })
+
     });
+
+    
   }
 
+  
+
   ngOnInit() {
+    this.user.subscribe(user => {
+      if (user) {
+        console.log('logged in');
+      } else {
+        
+        this.router.navigate(['/sign-in',{"routeParams":"checkout"}]);
+      }
+    });
     this._products.getPaymentGateways().subscribe(async (resp) => {
      console.log(resp)
     }, async (err) => {
@@ -81,15 +110,38 @@ export class CheckoutComponent implements OnInit {
 			message: 'Please wait...',
 		});
     loading.present();
+
+    
    
     this._products.appycoupon(this.todo.couponCode).subscribe(async (resp) => {
         loading.dismiss();	
         console.log(resp);
         this.couponData = resp;
-        let ins = this.cartProduct.price/100;
-        let dis = ins*this.couponData.price;
-        console.log(ins);
-        this.price = this.cartProduct.price-dis;
+        if(this.couponData.price !="0"){
+          this.couponAdded = 'true';
+          let ins = this.cartProduct.price/100;
+          let dis = ins*this.couponData.price;
+          console.log(ins);
+          this.price = this.cartProduct.price-dis;
+          loading.dismiss();		
+          const toast = await this.toastCtrl.create({
+              message: 'Coupon Added Successfully',
+              duration: 2000
+          });
+          toast.present();
+        }
+
+        else{
+
+          loading.dismiss();		
+          const toast = await this.toastCtrl.create({
+              message: 'Invalid Coupon Code',
+              duration: 2000
+          });
+          toast.present();
+
+        }
+        
     }, async (err) => {
         loading.dismiss();		
         const toast = await this.toastCtrl.create({
@@ -103,42 +155,51 @@ export class CheckoutComponent implements OnInit {
   payWithRazorpay() {
     //console.log(typeof(this.price));
     //this.createOrder();
-    var options = {
-      description: 'Credits towards consultation',
-      image: 'https://i.imgur.com/3g7nmJC.png',
-      currency: "INR", // your 3 letter currency code
-      key: "rzp_test_1DP5mmOlF5G5ag", // your Key Id from Razorpay dashboard
-      amount: this.price, // Payment amount in smallest denomiation e.g. cents for USD
-      name: 'Razorpay',
-     
-      prefill: {
-        email: 'test@razorpay.com',
-        contact: '9990009991',
-        name: 'Razorpay'
-      },
-      theme: {
-        color: '#F37254'
-      },
-      modal: {
-        ondismiss: function () {
-          alert('dismissed')
+
+    if(this.userid !=""){
+      var options = {
+        description: 'Credits towards consultation',
+        image: 'https://i.imgur.com/3g7nmJC.png',
+        currency: "INR", // your 3 letter currency code
+        key: "rzp_test_1DP5mmOlF5G5ag", // your Key Id from Razorpay dashboard
+        amount: this.price, // Payment amount in smallest denomiation e.g. cents for USD
+        name: 'Razorpay',
+       
+        prefill: {
+          email: 'test@razorpay.com',
+          contact: '9990009991',
+          name: 'Razorpay'
+        },
+        theme: {
+          color: '#F37254'
+        },
+        modal: {
+          ondismiss: function () {
+            alert('dismissed')
+          }
         }
-      }
-    };
+      };
+  
+      var successCallback = (payment_id) => {
+        console.log("razor success",payment_id);
+        this.storage.set("payment_id",payment_id);
+        this.createOrder(); 
+      };
+  
+      var cancelCallback = function (error) {
+        alert(error.description + ' (Error ' + error.code + ')');
+      };
+  
+      RazorpayCheckout.open(options);
+      RazorpayCheckout.on('payment.success', successCallback);
+      RazorpayCheckout.on('payment.cancel', cancelCallback);
+    }
 
-    var successCallback = (payment_id) => {
-      console.log("razor success",payment_id);
-      this.storage.set("payment_id",payment_id);
-      this.createOrder(); 
-    };
+    else{
+      this.router.navigate(['/sign-in',{"routeParams":"checkout"}]);
+    }
 
-    var cancelCallback = function (error) {
-      alert(error.description + ' (Error ' + error.code + ')');
-    };
-
-    RazorpayCheckout.open(options);
-    RazorpayCheckout.on('payment.success', successCallback);
-    RazorpayCheckout.on('payment.cancel', cancelCallback);
+    
   }
 
   async createOrder(){
@@ -157,51 +218,119 @@ export class CheckoutComponent implements OnInit {
     }
     //this.orderData.line_items.push(products);
 
-    let createOrderData = {
-      payment_method: "razorpay",
-      payment_method_title: "Razorpay",
-      set_paid: true,
-      discount_total: this.price,
-
-      billing: {
-        first_name: "",
-        last_name: "",
-        address_1: "",
-        address_2: "",
-        city: "",
-        state: "",
-        postcode: "",
-        country: "",
-        email: "",
-        phone: ""
-      },
-      shipping: {
-        first_name: "",
-        last_name: "",
-        address_1: "",
-        address_2: "",
-        city: "",
-        state: "",
-        postcode: "",
-        country: "",
-        email: "",
-        phone: ""
-      },
-      line_items: [
-        {
-          product_id: this.cartProduct.id,
-          quantity: 1,
+    if(this.couponAdded == "true"){
+      const createOrderData = {
+        payment_method: "razorpay",
+        payment_method_title: "Razorpay",
+        set_paid: true,
+        discount_total: this.price,
+  
+        billing: {
+          first_name: "",
+          last_name: "",
+          address_1: "",
+          address_2: "",
+          city: "",
+          state: "",
+          postcode: "",
+          country: "",
+          email: "",
+          phone: ""
+        },
+        shipping: {
+          first_name: "",
+          last_name: "",
+          address_1: "",
+          address_2: "",
+          city: "",
+          state: "",
+          postcode: "",
+          country: "",
+          email: "",
+          phone: ""
+        },
+        line_items: [
+          {
+            product_id: this.cartProduct.id,
+            quantity: 1,
+            
+          }
+        ],
+        coupon_lines:[
+          {
+            code:"LOVE2SHOP",
+          }
           
-        }
-      ],
-      coupon_lines:[
-        {
-          code:"LOVE2SHOP",
-        }
-        
+  
+        ]
+      }
 
-      ]
+    createOrderData['shipping'] = shillpingAddress;
+    createOrderData['billing'] = shillpingAddress;
+    createOrderData['customer_id'] = this.userid;
+    this.http.post("https://avatto.in/wp-json/wc/v3/orders?consumer_key=ck_ac58eb3d104d676d8c0543ac74b6ad5a3a3dd7a9&consumer_secret=cs_d7d0b84586275581ad4f307dc8f9e582fffee848", createOrderData)
+      .subscribe(
+        async data => {
+          loading.dismiss();
+          const toast = await this.toastCtrl.create({
+                message: 'Order has been successfully placed.',
+                duration: 2000
+          });
+          toast.present(); 
+          this.router.navigate(['/profile']);   
+          console.log("Order successfully created", data);
+        },
+        async error => {
+          console.log("Error", error);
+          const toast = await this.toastCtrl.create({
+                  message: 'failed to place order.',
+                  duration: 2000
+            });
+          toast.present();
+        }
+      );
     }
+
+    else{
+      const createOrderData = {
+        payment_method: "razorpay",
+        payment_method_title: "Razorpay",
+        set_paid: true,
+        discount_total: this.price,
+  
+        billing: {
+          first_name: "",
+          last_name: "",
+          address_1: "",
+          address_2: "",
+          city: "",
+          state: "",
+          postcode: "",
+          country: "",
+          email: "",
+          phone: ""
+        },
+        shipping: {
+          first_name: "",
+          last_name: "",
+          address_1: "",
+          address_2: "",
+          city: "",
+          state: "",
+          postcode: "",
+          country: "",
+          email: "",
+          phone: ""
+        },
+        line_items: [
+          {
+            product_id: this.cartProduct.id,
+            quantity: 1,
+            
+          }
+        ],
+        
+      }
 
     createOrderData['shipping'] = shillpingAddress;
     createOrderData['billing'] = shillpingAddress;
@@ -227,23 +356,7 @@ export class CheckoutComponent implements OnInit {
           toast.present();
         }
       );
-    
-    
-    // this._products.postOrder(this.orderData).subscribe(async (resp) => {
-    //   loading.dismiss();
-    //   const toast = await this.toastCtrl.create({
-    //     message: 'Order has been successfully placed.',
-    //     duration: 2000
-    //   });
-    //   toast.present();
-    //   //this.router.navigate(['/home']);
-    // }, async (err) => {
-    //     const toast = await this.toastCtrl.create({
-    //       message: 'failed to place order.',
-    //       duration: 2000
-    //     });
-    //     toast.present();
-    // });   
+    }
   }
 
   checkTerm(){
